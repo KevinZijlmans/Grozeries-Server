b4d19f077f85712203a83fd5ca3df05d6b87d09d
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-05-15 12:01:03 +0200

    started user routes

---
+++ users/routes.js
@@ -0,0 +1,109 @@
+const { Router } = require('express')
+const User = require('./model')
+const bcrypt = require('bcrypt')
+
+const router = new Router()
+  
+  router.post('/register', (req, res, next) => {
+    const user = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        address: req.body.address,
+        phonenumber: req.body.phonenumber,
+        user_type: req.body.user_type       
+    }
+    User
+      .create(user)
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: `user does not exist`
+          })
+        }
+        return res.status(201).send(user)
+      })
+      .catch(error => next(error))
+  })
+
+  router.get('/users/:id', (req, res, next) => {
+    User
+      .findByPk(req.params.id) 
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: `customer does not exist`
+          })
+        }
+        return res.send(user)
+      })
+      .catch(error => next(error))
+  })
+
+  router.get('/users/sellers', (req, res, next) => {
+    User
+        .findAll(
+            { where: {user_type: 'Seller'} }
+        )
+        .then(users => {
+            res.json({ users: users })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Something went wrong',
+                error: err
+            })
+        })
+ })
+
+ router.get('/users/customers', (req, res, next) => {
+    User
+        .findAll(
+            { where: {user_type: 'Customer'} }
+        )
+        .then(users => {
+            res.json({ users: users })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Something went wrong',
+                error: err
+            })
+        })
+ })
+
+ router.get('/users/drivers', (req, res, next) => {
+    User
+        .findAll(
+            { where: {user_type: 'Driver'} }
+        )
+        .then(users => {
+            res.json({ users: users })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Something went wrong',
+                error: err
+            })
+        })
+ })
+
+ router.put('/users/:id', (req, res, next) => {
+    User
+        .findByPk(req.params.id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: `User does not exist`
+                })
+            }
+            return user.update(req.body)
+                .then(user => res.send(user))
+        })
+        .catch(error => next(error))
+ })
+
+ router.
+
+  module.exports = router

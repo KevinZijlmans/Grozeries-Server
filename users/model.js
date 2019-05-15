8e225df9a7edd68a8920229b169177111e3560d9
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-05-15 10:16:21 +0200

    first commit

---
+++ users/model.js
@@ -0,0 +1,42 @@
+const Sequelize = require('../node_modules/sequelize')
+const sequelize = require('../db')
+
+const User = sequelize.define('users', {
+    first_name: {
+        type: Sequelize.STRING,
+        field: 'first_name',
+        allowNull: false
+    },
+    last_name: {
+        type: Sequelize.STRING,
+        field: 'last_name',
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        field: 'email',
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        field: 'password',
+        allowNull: false
+    },
+    address: {
+        type: Sequelize.STRING,
+        field: 'address',
+        allowNull: false
+    },
+    phonenumber: {
+        type: Sequelize.STRING,
+        field: 'phonenumber',
+        allowNull: false
+    },
+    user_type: {
+        type: Sequelize.STRING,
+        field: 'user_type',
+        allowNull: false
+    },
+  })
+
+  module.exports = User

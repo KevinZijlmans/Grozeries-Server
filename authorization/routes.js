8834e78a14acfe75009fad6cb9e90e342b397168
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-05-15 11:30:13 +0200

    added authorization route

---
+++ authorization/routes.js
@@ -0,0 +1,54 @@
+const { Router } = require('express')
+const { toJWT }= require('./jwt')
+const User = require('../users/model')
+const bcrypt = require('bcrypt')
+
+
+const router = new Router()
+
+router.post('/logins', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    res.status(400).send({
+      message: 'Please supply a valid email and password'
+    })
+  }
+  else {
+    User
+      .findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: 'User with that email does not exist'
+          })
+        }
+        if (bcrypt.compareSync(req.body.password, entity.password)) {
+          res.send({
+            jwt: toJWT({ userId: entity.id }),
+            first_name: entity.first_name,
+            last_name: entity.last_name,
+            id: entity.id
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Password was incorrect'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Something went wrong'
+        })
+      })
+  }
+})
+  
+module.exports = router

f430b1835595446b68bbfb7fa246226d3d448d11
robertlangereis <robert.langereis@gmail.com>
2019-05-20 11:20:32 +0200

    completely removed the route and model of payments. Everything is implemented into the order model and routes

--- orders/routes.js
+++ orders/routes.js
@@ -3,6 +3,7 @@ const Order = require('./model')
 const Orderline = require('../orderlines/model')
 const Product = require('../products/model')
 const auth = require("../authorization/middleware")
+const mollie = require('@mollie/api-client')({ apiKey: 'test_qcMAbRrhuVzzkVaR6DRMgDq86k8NWt' });
 
 
 const router = new Router()
@@ -74,6 +75,54 @@ router.put('/orders/:id', auth, (req, res, next) => {
                 .then(order => res.send(order))
         })
         .catch(error => next(error))
+})
+
+router.post('/orders/:id/payments', (req, res, next) => {
+    const orderId = req.params.id
+    // const orderAmount = req.payment_amount
+    const orderAmount = '100'
+    Order
+    .findByPk(orderId)
+    .then((order) => {
+        if (!order) {
+            return res.status(404).send({
+                message: `order does not exist`
+            }) 
+        } order.payment_started = true
+        console.log("Order 1 changed payment started to true", order)
+        order.save()
+        return res.status(201).send({
+            message: `Order payment initiated`
+        })
+    })
+    .catch(error => next(error))
+    
+    mollie.payments
+        .create({
+            "amount": {
+                "value": "13.00",
+                "currency": "EUR"
+            },
+            "description": `Grozeries Payment with orderId: ${orderId} and with orderAmount: ${orderAmount}`,
+            "redirectUrl": "http://localhost:3000/products/1",
+            // "webhookUrl":  `http://localhost:4000/payments/${orderId}/webhook/`,
+            "webhookUrl":  "http://albertsm.it/",
+            "method": "ideal"
+            })
+        .then((payment) => {
+            // console.log(payment)
+            if (!payment) {
+                return res.status(404).send({
+                    message: `Mollie payment does not exist`
+                })
+            }
+            // console.log("URL-URL",payment.getPaymentUrl())
+            return payment.getPaymentUrl()
+            })
+        .catch((err) => {
+            console.log("ERROR: ",err)
+            // Handle the error
+            });
 })
 
 module.exports = router
--- payments/routes.js
+++ payments/routes.js
@@ -3,57 +3,11 @@ const Order = require('../orders/model')
 // const Orderline = require('../orderlines/model')
 // const Payment = require('../payments/model')
 // const auth = require("../authorization/middleware")
-const mollie = require('@mollie/api-client')({ apiKey: 'test_qcMAbRrhuVzzkVaR6DRMgDq86k8NWt' });
+
 
 const router = new Router()
 
-router.post('/orders/:id/payments', (req, res, next) => {
-    const orderId = req.params.id
-    // const orderAmount = req.payment_amount
-    const orderAmount = '100'
-    Order
-    .findByPk(orderId)
-    .then((order) => {
-        if (!order) {
-            return res.status(404).send({
-                message: `order does not exist`
-            }) 
-        } order.payment_started = true
-        console.log("Order 1 changed payment started to true", order)
-        order.save()
-        return res.status(201).send({
-            message: `Order payment initiated`
-        })
-    })
-    .catch(error => next(error))
-    
-    mollie.payments
-        .create({
-            "amount": {
-                "value": "12.00",
-                "currency": "EUR"
-            },
-            "description": `Grozeries Payment with orderId: ${orderId} and with orderAmount: ${orderAmount}`,
-            "redirectUrl": "http://localhost:3000/products/1",
-            // "webhookUrl":  `http://localhost:4000/payments/${orderId}/webhook/`,
-            "webhookUrl":  "http://albertsm.it/",
-            "method": "ideal"
-            })
-        .then((payment) => {
-            // console.log(payment)
-            if (!payment) {
-                return res.status(404).send({
-                    message: `Mollie payment does not exist`
-                })
-            }
-            // console.log("URL-URL",payment.getPaymentUrl())
-            return payment.getPaymentUrl()
-            })
-        .catch((err) => {
-            console.log("ERROR: ",err)
-            // Handle the error
-            });
-})
+
 
 // router.post(`payments/${paymentId}/webhook/`, auth, (req, res, next) => {
 //     const orderId = req.params.id

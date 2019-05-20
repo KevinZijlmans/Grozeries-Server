561692bc7817d933b2aacea6c19fc0fbf70192f0
Vivian da Graca <vivian.da.graca@gmail.com>
2019-05-20 15:03:20 +0200

    nullable order

--- migrations/20190516134702-create-order.js
+++ migrations/20190516134702-create-order.js
@@ -9,27 +9,35 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       street_name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       house_number: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       zipcode: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       city: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       comments: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       delivery_time: {
+        allowNull: false,
         type: Sequelize.DATE
       },
       status: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       payment_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
